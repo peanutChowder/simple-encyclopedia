a24085a58ac093225b573e668ad43c7f55671c3b
jacob <peanutchowder@gmail.com>
2021-06-10 11:16:01 -0700

    added search functionality

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -4,5 +4,9 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:pageName>", views.wikiPage, name="wikiPage")
+    path("wiki/<str:pageName>", views.wikiPage, name="wikiPage"),
+
+    path("tester", views.tester, name="tester"),
+    path("search", views.search, name="search-page")
+
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,16 +1,35 @@
+from logging import PlaceHolder
 from django.http.response import HttpResponse
 from django.shortcuts import render
 from . import util
 
 from markdown2 import Markdown
+from django import forms
 
 markdowner = Markdown()
 
+class SearchForm(forms.Form):
+    searchQuery = forms.CharField()
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+def search(request):
+    if request.method == "POST":
+        searched = request.POST['searched']
+        matchingEntries = [entry for entry in util.list_entries() if searched.lower() in entry.lower()]
+        
+        return render(request, "encyclopedia/search.html", {
+            'searched': searched,
+            "matchingEntries": matchingEntries
+        })
+    else:
+        return render(request, "encyclopedia/search.html", {})
+
+def tester(request):
+    return HttpResponse("hello")
 
 def wikiPage(request, pageName):
     pageMarkdown = open(f"entries/{pageName}.md", "r")
